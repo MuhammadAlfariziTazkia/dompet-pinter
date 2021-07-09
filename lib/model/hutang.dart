a452dd7fca2f63920313d5b1e9d54b9226018b49
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2021-07-09 15:05:40 +0700

    add class for hutang data

---
+++ lib/model/hutang.dart
@@ -0,0 +1,20 @@
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 1)
+class Hutang {
+  @HiveField(0)
+  String pemberi;
+  @HiveField(1)
+  String tanggal;
+  @HiveField(2)
+  String penerima;
+  @HiveField(3)
+  int nominal;
+  @HiveField(4)
+  String keperluan;
+  @HiveField(5)
+  String waktuLunas;
+
+  Hutang(this.keperluan, this.nominal, this.pemberi, this.penerima,
+      this.tanggal, this.waktuLunas);
+}
